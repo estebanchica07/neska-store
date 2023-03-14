80d0622fb352ed8183ab73e9fc9eee4a8ae87a2e
estebanchica07 <estebanchica07@gmail.com>
2023-03-14 17:12:25 -0500

    Open and close menus

--- neska.js
+++ neska.js
@@ -2,12 +2,16 @@ const cuentaEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const botonMenu = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const myOrder = document.querySelector(".product-detail");
+const cartBoton = document.querySelector(".navbar-shopping-cart");
 
 cuentaEmail.addEventListener("click", () => toggleElement(desktopMenu));
-botonMenu.addEventListener("click", () => toggleElement(mobileMenu));
+botonMenu.addEventListener("click", () => abrirCerrar(mobileMenu, myOrder));
+cartBoton.addEventListener("click", () => abrirCerrar(myOrder, mobileMenu));
 
-const toggleElement = (e) => {
+const abrirCerrar = function (e, m) {
   e.classList.toggle("inactive");
+  m.classList.add("inactive");
 };
 
 // function switchMenu() {
