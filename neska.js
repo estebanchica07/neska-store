7be65c009e67bb1558b45ae595e339c83ee0968a
estebanchica07 <estebanchica07@gmail.com>
2023-03-14 14:51:04 -0500

    CSS updated, shoes menus addes

--- neska.js
+++ neska.js
@@ -1,8 +1,15 @@
 const cuentaEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const botonMenu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 cuentaEmail.addEventListener("click", switchMenu);
+botonMenu.addEventListener("click", appearMenu);
 
 function switchMenu() {
   desktopMenu.classList.toggle("inactive");
+}
+
+function appearMenu() {
+  mobileMenu.classList.toggle("inactive");
 }
