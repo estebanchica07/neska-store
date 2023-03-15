169c7e0cbdee1fa19a637d2f4890a70a1af4b886
estebanchica07 <estebanchica07@gmail.com>
2023-03-14 23:10:33 -0500

    function trhee parameters

--- neska.js
+++ neska.js
@@ -5,13 +5,20 @@ const mobileMenu = document.querySelector(".mobile-menu");
 const myOrder = document.querySelector(".product-detail");
 const cartBoton = document.querySelector(".navbar-shopping-cart");
 
-cuentaEmail.addEventListener("click", () => toggleElement(desktopMenu));
-botonMenu.addEventListener("click", () => abrirCerrar(mobileMenu, myOrder));
-cartBoton.addEventListener("click", () => abrirCerrar(myOrder, mobileMenu));
+cuentaEmail.addEventListener("click", () =>
+  abrirCerrar(desktopMenu, myOrder, mobileMenu)
+);
+botonMenu.addEventListener("click", () =>
+  abrirCerrar(mobileMenu, myOrder, desktopMenu)
+);
+cartBoton.addEventListener("click", () =>
+  abrirCerrar(myOrder, mobileMenu, desktopMenu)
+);
 
-const abrirCerrar = function (e, m) {
+const abrirCerrar = function (e, m, g) {
   e.classList.toggle("inactive");
   m.classList.add("inactive");
+  g.classList.add("inactive");
 };
 
 // function switchMenu() {
